d5446e2c5a8817b2f2baf7252bd96ebc6d3b0add
MarkoVrljicak <marcos.vrljicak@gmail.com>
2016-05-13 12:21:36 -0300

    Entidades para que nos pongamos de acuerdo

---
+++ entities.h
@@ -0,0 +1,29 @@
+//
+// Created by marcos on 13/05/16.
+//
+
+#ifndef MEGAMAN_ENTITIES_H
+#define MEGAMAN_ENTITIES_H
+
+//Mobs
+#define MET 1001
+#define BUMBY 1002
+#define SNIPER 1003
+#define JUMPING_SNIPER 1004
+
+//Bosses
+#define BOMBMAN 2001
+#define SPARKMAN 2002
+#define MAGNETMAN 2003
+#define RINGMAN 2004
+#define FIREMAN 2005
+
+//Tiles
+#define SPIKE 3001
+#define LADDER 3004
+#define BASIC_TILE 3005
+
+//Backgrounds
+#define METALLIC_00 define 4001
+
+#endif //MEGAMAN_ENTITIES_H
